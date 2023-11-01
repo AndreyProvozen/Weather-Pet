65abcbb0bd85f7b88d227279c2a0b8babd687be8
Andrey Provozen <provozena885@gmail.com>
2023-11-01 15:49:19 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <Navigation />
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import Navigation from "./components/Navigation.vue";
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navigation">
+    <h1 class="logo">Weather-Pet</h1>
+    <div class="right-navigation">
+      <a href="#">Main</a>
+      <a href="#">Test</a>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.navigation {
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+  background-color: $color-header-footer-bg;
+  justify-content: space-between;
+}
+
+.logo {
+  margin: 0;
+}
+
+.right-navigation {
+  display: flex;
+  align-items: center;
+  column-gap: 15px;
+  justify-content: flex-end;
+}
+</style>
