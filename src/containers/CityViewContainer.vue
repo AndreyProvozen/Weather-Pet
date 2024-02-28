86f40543e3b815f989698f882e67c3766fd2dcba
Andrey Provozen <provozena885@gmail.com>
2024-02-27 19:44:57 -0700

    Updated interface folder

--- src/containers/CityViewContainer.vue
+++ src/containers/CityViewContainer.vue
@@ -10,41 +10,14 @@
 </template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
 import LeftWeather from '@/components/CityViewComponents/LeftWeather';
 import RightWeatherContent from '@/components/CityViewComponents/RightWeatherContent.vue';
 
-const route = useRoute();
+interface Props {
+  weatherData: any;
+}
 
-const getWeatherData = async () => {
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lon}&appid=${
-        import.meta.env.VITE_APP_WEATHER_API_KEY
-      }&units=metric&exclude=minutely`
-    );
-
-    const weatherResponse = await response.json();
-
-    const localOffset = new Date().getTimezoneOffset() * 60000;
-
-    const utc = weatherResponse.current?.dt * 1000 + localOffset;
-
-    weatherResponse.currentTime = utc + 1000 * weatherResponse.timezone_offset;
-
-    weatherResponse.hourly.forEach((hour: any) => {
-      const utc = hour.dt * 1000 + localOffset;
-      hour.currentTime = utc + 1000 * weatherResponse.timezone_offset;
-    });
-
-    return weatherResponse;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const weatherData = await getWeatherData();
-console.log(weatherData);
+defineProps<Props>();
 </script>
 
 <style scoped>
--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,15 +1,46 @@
 <template>
-  <Suspense>
-    <template #default>
-      <CityViewContainer />
-    </template>
-    <template #fallback>
-      <Loader />
-    </template>
-  </Suspense>
+  <CityViewContainer v-if="weatherData" :weather-data="weatherData" />
+  <Loader v-else />
 </template>
 
 <script setup lang="ts">
 import CityViewContainer from '@/containers/CityViewContainer.vue';
 import Loader from '@/atoms/Loader.vue';
+import { useRoute } from 'vue-router';
+import { onMounted, ref } from 'vue';
+
+const route = useRoute();
+
+const weatherData = ref(null);
+
+const getWeatherData = async () => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lon}&appid=${
+        import.meta.env.VITE_APP_WEATHER_API_KEY
+      }&units=metric&exclude=minutely`
+    );
+
+    const weatherResponse = await response.json();
+
+    const localOffset = new Date().getTimezoneOffset() * 60000;
+
+    const utc = weatherResponse.current?.dt * 1000 + localOffset;
+
+    weatherResponse.currentTime = utc + 1000 * weatherResponse.timezone_offset;
+
+    weatherResponse.hourly.forEach((hour: any) => {
+      const utc = hour.dt * 1000 + localOffset;
+      hour.currentTime = utc + 1000 * weatherResponse.timezone_offset;
+    });
+
+    weatherData.value = weatherResponse;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(async () => {
+  await getWeatherData();
+});
 </script>
