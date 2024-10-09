dcaa5fbc2b49955bd11dd97ef4609e970c2bd710
Andrey Provozen <provozena885@gmail.com>
2024-10-08 21:11:34 -0600

    fix: resolve build error

--- vite.config.ts
+++ vite.config.ts
@@ -29,14 +29,5 @@ export default defineConfig({
   css: { preprocessorOptions: { scss: { api: 'modern', additionalData: '@import "@/styles/index.scss";' } } },
   build: {
     target: 'esnext',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
   },
 });
